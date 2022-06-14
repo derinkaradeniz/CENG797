5e32d002e8b96a7dcc9cd80933897e0ffd2a87cc
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-14 19:39:21 +0300

    mainapp

--- PhysicalLayers/mainApp.py
+++ PhysicalLayers/mainApp.py
@@ -1,107 +1,66 @@
 import os
 import sys
-import time, random, math
-from enum import Enum
-from pickle import FALSE
+import time
 
-from videoStream import OpenCVVideoStreamingApp, OpenCVVideoStreamingAppEventTypes
-from csmaPlain import CsmaPlain, MacCsmaPPersistentConfigurationParameters, CsmaPlain
 from adhoccomputing.GenericModel import GenericModel
-from adhoccomputing.Generics import Event, EventTypes, ConnectorTypes, GenericMessageHeader,GenericMessage
+from adhoccomputing.Generics import *
 from adhoccomputing.Experimentation.Topology import Topology
+from adhoccomputing.Networking.LinkLayer.GenericLinkLayer import GenericLinkLayer
+from adhoccomputing.Networking.NetworkLayer.GenericNetworkLayer import GenericNetworkLayer
+from adhoccomputing.Networking.LogicalChannels.GenericChannel import GenericChannel
+from adhoccomputing.Networking.ApplicationLayer.OpenCVVideoStreamingApp import *
 from adhoccomputing.Networking.PhysicalLayer.UsrpB210OfdmFlexFramePhy import  UsrpB210OfdmFlexFramePhy
-#from adhoccomputing.Networking.MacProtocol.CSMA import MacCsmaPPersistent, MacCsmaPPersistentConfigurationParameters
-
-#registry = ComponentRegistry()
-#from ahc.Channels.Channels import FIFOBroadcastPerfectChannel
-#from ahc.EttusUsrp.UhdUtils import AhcUhdUtils
-
-#framers = FramerObjects()
+from adhoccomputing.Networking.MacProtocol.CSMA import MacCsmaPPersistent, MacCsmaPPersistentConfigurationParameters
+from adhoccomputing.Networking.ApplicationLayer.MessageSegmentation import *
+from gpsHandler import *
+from communicator import *
+import logging
 
 
-# define your own message types
-class ApplicationLayerMessageTypes(Enum):
-    BROADCAST = "BROADCAST"
-    VIDEO = "VIDEO"
+macconfig = MacCsmaPPersistentConfigurationParameters(0.5, -50)
+#sdrconfig = SDRConfiguration(freq =915000000.0, bandwidth = 4000000, chan = 0, hw_tx_gain = 70, hw_rx_gain = 30, sw_tx_gain = -12.0)
+sdrconfig = SDRConfiguration(freq =915000000.0, bandwidth = 20000000, chan = 0, hw_tx_gain = 76, hw_rx_gain = 20, sw_tx_gain = -12.0)
 
 
-# define your own message header structure
-class ApplicationLayerMessageHeader(GenericMessageHeader):
-    pass
 
+class AdHocNode(GenericModel):
 
-class UsrpApplicationLayerEventTypes(Enum):
-    STARTBROADCAST = "startbroadcast"
-    STARTSTREAMING = "startstreaming"
-
-
-class UsrpApplicationLayer(GenericModel):
     def on_init(self, eventobj: Event):
-        self.counter = 0
-    
-    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
-        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
-        self.eventhandlers[UsrpApplicationLayerEventTypes.STARTBROADCAST] = self.on_startbroadcast
+        logger.applog(f"Initializing {self.componentname}.{self.componentinstancenumber}")
+        pass
 
     def on_message_from_top(self, eventobj: Event):
-    # print(f"I am {self.componentname}.{self.componentinstancenumber},sending down eventcontent={eventobj.eventcontent}\n")
-        self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
-    
+        self.send_down(eventobj)
+
     def on_message_from_bottom(self, eventobj: Event):
-        evt = Event(self, EventTypes.MFRT, eventobj.eventcontent)
-        print(f"I am Node.{self.componentinstancenumber}, received from Node.{eventobj.eventcontent.header.messagefrom} a message: {eventobj.eventcontent.payload}")    
-        if self.componentinstancenumber == 1:
-            evt.eventcontent.header.messageto = 0
-            evt.eventcontent.header.messagefrom = 1
-        else:
-            evt.eventcontent.header.messageto = 1
-            evt.eventcontent.header.messagefrom = 0
-        evt.eventcontent.payload = eventobj.eventcontent.payload
-        #print(f"I am {self.componentname}.{self.componentinstancenumber}, sending down eventcontent={eventobj.eventcontent.payload}\n")
-        self.send_down(evt)  # PINGPONG
-    
-    def on_startbroadcast(self, eventobj: Event):
-        #if self.componentinstancenumber == 1:
-        #   hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 1, 0)
-        #else:
-        #    hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 0, 1)
-        hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, self.componentinstancenumber, 1)
-        self.counter = self.counter + 1
-        
-        payload = "BMSG-" + str(self.counter)
-        broadcastmessage = GenericMessage(hdr, payload)
-        evt = Event(self, EventTypes.MFRT, broadcastmessage)
-        # time.sleep(3)
-        self.send_down(evt)
-        #print("Starting broadcast")
-    
-         
-class UsrpNode(GenericModel):
-    counter = 0
-    def on_init(self, eventobj: Event):
-        pass
-    
+        self.send_up(eventobj)
+
     def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
         super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
         # SUBCOMPONENTS
         
-        macconfig = MacCsmaPPersistentConfigurationParameters(0.5)
-        
-        OpenCVVideoStreamingApp
-        #self.appl = UsrpApplicationLayer("UsrpApplicationLayer", componentinstancenumber, topology=topology)
-        self.appl = OpenCVVideoStreamingApp("OpenCVVideoStreamingApp", componentinstancenumber, topology=topology)
-        self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, topology=topology)
-        self.mac = CsmaPlain("MacCsmaPPersistent", componentinstancenumber,  configurationparameters=macconfig, uhd=self.phy.sdrdev,topology=topology)
-        
-        self.components.append(self.appl)
-        self.components.append(self.phy)
-        self.components.append(self.mac)
+        self.gpsApp = = GPSHandlerApp("GPSHandlerApp", componentinstancenumber, topology=topology)
+        self.appl = CommunicatorApp("CommunicatorApp", componentinstancenumber, topology=topology)
+        self.seg = MessageSegmentation("MessageSegmentation", componentinstancenumber, topology=topology)
+        self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, topology=topology,usrpconfig=sdrconfig, )
+        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentinstancenumber,  configurationparameters=macconfig, sdr=self.phy.sdrdev, topology=topology)
 
-        # CONNECTIONS AMONG SUBCOMPONENTS
-        self.appl.connect_me_to_component(ConnectorTypes.UP, self) #Not required if nodemodel will do nothing
-        self.appl.connect_me_to_component(ConnectorTypes.DOWN, self.mac)
+        self.components.append(self.gpsApp)
+        self.components.append(self.appl)
+        self.components.append(self.mac)
+        self.components.append(self.seg)
+        self.components.append(self.phy)
         
-        self.mac.connect_me_to_component(ConnectorTypes.UP, self.appl)
+        # CONNECTIONS AMONG SUBCOMPONENTS
+        self.gpsApp.connect_me_to_component(ConnectorTypes.PEER, self.appl) 
+
+        self.appl.connect_me_to_component(ConnectorTypes.PEER, self.gpsApp)
+        self.appl.connect_me_to_component(ConnectorTypes.DOWN, self.seg)
+
+        self.seg.connect_me_to_component(ConnectorTypes.UP, self.appl)
+        self.seg.connect_me_to_component(ConnectorTypes.DOWN, self.mac)
+        
+        self.mac.connect_me_to_component(ConnectorTypes.UP, self.seg)
         self.mac.connect_me_to_component(ConnectorTypes.DOWN, self.phy)
         
         # Connect the bottom component to the composite component....
@@ -110,7 +69,6 @@ class UsrpNode(GenericModel):
         
         # self.phy.connect_me_to_component(ConnectorTypes.DOWN, self)
         # self.connect_me_to_component(ConnectorTypes.DOWN, self.appl)
-    
         
 
 def main():
@@ -127,7 +85,7 @@ def main():
     i = 0
     while(i < 3):
         #topo.nodes[3].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
-        topo.nodes[3].appl.send_self(Event(topo.nodes[0], OpenCVVideoStreamingAppEventTypes.STARTSTREAMING, None))
+        topo.nodes[3].appl.send_self(Event(topo.nodes[0], CommunicatorAppEventTypes.STARTGPSREQ, None))
 
         time.sleep(1)
         i = i + 1
