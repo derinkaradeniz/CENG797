64d0adf9c960cadd8779619b8836a5c184a9e92e
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-14 21:56:06 +0300

    .

--- PhysicalLayers/communicator.py
+++ PhysicalLayers/communicator.py
@@ -60,6 +60,7 @@ class CommunicatorApp(GenericModel):
         elif eventobj.eventcontent.header.messagetype == GPSHandlerAppMessageTypes.DISTANCE:
             print("comm: from peer: distance")
             distance = eventobj.eventcontent.payload
+            print(f"Distance at comm: {str(distance)}")
             if distance < 200:
                 header = CommunicatorAppMessageHeader(CommunicatorAppMessageTypes.TEXTMESSAGE, eventobj.eventcontent.header.messagefrom, eventobj.eventcontent.header.messageto)     
                 payload = loremIpsum
