0c061a7ade0ff98aab4df90733c7ce3a56aa9562
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-14 19:31:40 +0300

    gpshandler and communicator

---
+++ PhysicalLayers/communicator.py
@@ -0,0 +1,64 @@
+import time
+import random
+
+from ...Generics import *
+from ...GenericModel import GenericModel
+import pickle
+
+from lorem import *
+
+# define your own message types
+class CommunicatorAppMessageTypes(Enum):
+    LOCATION = "LOCATION"
+    ISLOCATION = "ISLOCATION"
+    DISTANCE = "ISDISTANCE"
+    TEXTMESSAGE = "TEXTMESSAGE"
+
+# define your own message header structure
+class CommunicatorAppMessageHeader(GenericMessageHeader):
+    pass
+
+class CommunicatorAppEventTypes(Enum):    
+    STARTGPSREQ = "startspsreq"
+
+class CommunicatorApp(GenericModel):
+    myLocation = [0]*2
+    def on_init(self, eventobj: Event):
+        self.counter = 0       
+    
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
+        self.eventhandlers[CommunicatorAppEventTypes.STARTGPSREQ] = self.on_startgpsreq
+
+     def on_message_from_bottom(self, eventobj: Event):
+        if eventobj.eventcontent.hdr.messagetype == "LOCATION"
+            hdr = CommunicatorAppMessageHeader(CommunicatorAppMessageHeader.ISDISTANCE, eventobj.eventcontent.hdr.messagefrom, eventobj.eventcontent.hdr.messageto)     
+            payload = eventobj.eventcontent.payload
+            message = GenericMessage(hdr, payload) 
+            evt = Event(self, EventTypes.MFRP, message)
+            self.send_peer(evt)
+
+    def on_message_from_peer(self, eventobj: Event):
+        if eventobj.eventcontent.hdr.messagetype == "LOCATION"
+            hdr = CommunicatorAppMessageHeader(CommunicatorAppMessageHeader.LOCATION, self.componentinstancenumber, eventobj.eventcontent.hdr.messageto)     
+            payload = eventobj.eventcontent.payload
+            message = GenericMessage(hdr, payload) 
+            evt = Event(self, EventTypes.MFRT, message)
+            self.send_down(evt)
+        elif eventobj.eventcontent.hdr.messagetype == "DISTANCE":
+            distance = eventobj.eventcontent.payload
+            if payload < 10:
+                hdr = CommunicatorAppMessageHeader(CommunicatorAppMessageHeader.TEXTMESSAGE, eventobj.eventcontent.hdr.messagefrom, eventobj.eventcontent.hdr.messageto)     
+                payload = loremIpsum
+                message = GenericMessage(hdr, payload) 
+                evt = Event(self, EventTypes.MFRP, message)
+                self.send_peer(evt)
+
+
+    def on_startgpsreq(self, eventobj: Event):
+        hdr = CommunicatorAppMessageTypes(GPSHandlerAppMessageTypes.ISLOCATION, self.componentinstancenumber, self.componentinstancenumber)     
+        payload = "location request"
+        message = GenericMessage(hdr, payload)
+        evt = Event(self, EventTypes.MFRP, message)
+        self.send_peer(evt)
+
---
+++ PhysicalLayers/gpsHandler.py
@@ -0,0 +1,57 @@
+import time
+import random
+
+from ...Generics import *
+from ...GenericModel import GenericModel
+import pickle
+
+# define your own message types
+class GPSHandlerAppMessageTypes(Enum):
+    LOCATION = "LOCATION"
+    DISTANCE = "DISTANCE"
+
+# define your own message header structure
+class GPSHandlerAppMessageHeader(GenericMessageHeader):
+    pass
+
+
+class GPSHandlerAppEventTypes(Enum):
+    pass
+    
+
+#class GPSHandlerAppConfig:
+#    def __init__(self, framerate):
+#        self.framerate = framerate
+
+class GPSHandlerApp(GenericModel):
+    myLocation = [0]*2
+    def on_init(self, eventobj: Event):
+        self.counter = 0       
+    
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
+
+        self.myLocation[0] = random.random() * 360 - 180
+        self.myLocation[1] = random.random() * 180 - 90
+        logger.applog(f"My Location {str(self.myLocation[0])} , {str(self.myLocation[1])}")
+
+    def on_message_from_peer(self, eventobj: Event):
+        #evt = Event(self, EventTypes.MFRT, eventobj.eventcontent)
+        #hesaplama yap
+        if eventobj.eventContent.hdr.messagetype = "ISLOCATION":  
+            hdr = GPSHandlerAppMessageHeader(GPSHandlerAppMessageTypes.LOCATION, self.componentinstancenumber, eventobj.eventcontent.hdr.messagefrom)     
+            payload = self.myLocation
+            message = GenericMessage(hdr, payload)
+            evt = Event(self, EventTypes.MFRP, message)
+            self.send_peer(evt)
+
+        elif eventobj.eventContent.hdr.messagetype = "ISDISTANCE":
+            nodeLocation = eventobj.eventContent.payload
+            distance = sqrt((self.myLocation[0] - nodeLocation[0])**2 + (self.myLocation[1] - nodeLocation[1])**2)
+
+            hdr = GPSHandlerAppMessageHeader(GPSHandlerAppMessageTypes.DISTANCE, self.componentinstancenumber, eventobj.eventcontent.hdr.messagefrom)     
+            payload = distance
+            message = GenericMessage(hdr, payload)
+            evt = Event(self, EventTypes.MFRP, message) 
+            self.send_peer(evt)
+            logger.applog(f"{self.componentname}.{self.componentinstancenumber}: My distance from {eventobj.eventcontent.hdr.messagefrom} is {str(distance)}")
