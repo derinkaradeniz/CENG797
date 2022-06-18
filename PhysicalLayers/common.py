eb21baa9761ab14adcb1266aad5371e4861ca5a4
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-18 07:19:36 +0300

    .

--- PhysicalLayers/common.py
+++ PhysicalLayers/common.py
@@ -10,3 +10,10 @@ class CommunicatorAppMessageTypes(Enum):
 class GPSHandlerAppMessageTypes(Enum):
     LOCATION = "LOCATION"
     DISTANCE = "DISTANCE"
+
+nodeGPSLocations = [
+    [20,10],
+    [40,30],
+    [95,45],
+    [30,70]
+]
