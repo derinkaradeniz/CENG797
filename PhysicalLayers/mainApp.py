02bbbe5f41769ce08c73fd9c24e563199c2832af
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-18 10:21:57 +0300

    .

--- PhysicalLayers/mainApp.py
+++ PhysicalLayers/mainApp.py
@@ -102,7 +102,7 @@ def main():
             #topo.nodes[3].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
             topo.nodes[j].appl.send_self(Event(topo.nodes[0], CommunicatorAppEventTypes.STARTGPSREQ, None))
 
-            time.sleep(1)
+            #time.sleep(1)
             j = j + 1
         i = i + 1
 
