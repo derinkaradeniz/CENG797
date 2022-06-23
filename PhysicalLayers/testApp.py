64b914796d5f30b3e344c79720e66c87ef24af39
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-24 02:47:49 +0300

    testApp created

---
+++ PhysicalLayers/testApp.py
@@ -0,0 +1,49 @@
+import time
+import random
+from pathlib import Path
+
+from adhoccomputing.GenericModel import GenericModel
+from adhoccomputing.Generics import *
+from common import *
+from lorem import *
+
+class TestAppMessageHeader(GenericMessageHeader):
+    pass
+
+class TestAppEventTypes(Enum):    
+    STARTGPSREQ = "startreq"
+
+class TestAppMessageTypes(Enum):
+    BURST = "burst"
+
+class TestApp(GenericModel):
+    myLocation = [0]*2
+    def on_init(self, eventobj: Event):
+        #print("comm: on init")
+        self.counter = 0       
+    
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+        #print("comm: init")
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
+        self.eventhandlers[TestAppEventTypes.STARTREQ] = self.on_startreq
+
+    def on_message_from_bottom(self, eventobj: Event):
+        #print("comm: from bottom")
+        print(f"Node {self.componentinstancenumber}: received {eventobj.eventcontent.header.sequencenumber} from Node {eventobj.eventcontent.header.messageto}.")
+        
+
+    def on_message_from_peer(self, eventobj: Event):        
+        #print("comm: from peer")
+        pass
+        
+
+    def on_startreq(self, eventobj: Event):
+        #print("on_startreq")
+        header = TestAppMessageHeader(TestAppMessageTypes.BURST, self.componentinstancenumber, eventobj.eventcontent.header.messageto)
+        payload = bytearray([1] * 64)
+        message = GenericMessage(header, payload)
+        evt = Event(self, EventTypes.MFRT, message)
+
+        for i in range(20):
+            evt.eventcontent.header.sequencenumber = i + 1
+            self.send_down(evt)
