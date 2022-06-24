1f13216eb8f096da2bcdb8e7894ab8c0abd1f9d1
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-24 03:24:34 +0300

    .

--- PhysicalLayers/testApp.py
+++ PhysicalLayers/testApp.py
@@ -58,7 +58,7 @@ class TestApp(GenericModel):
 
         for i in range(20):
             header = TestAppMessageHeader(TestAppMessageTypes.BURST, self.componentinstancenumber, 0,sequencenumber= i + 1)
-            payload = bytearray([1] * 128)
+            payload = bytearray([1] * 512)
             message = GenericMessage(header, payload)
             evt = Event(self, EventTypes.MFRT, message)
             self.send_down(evt)
