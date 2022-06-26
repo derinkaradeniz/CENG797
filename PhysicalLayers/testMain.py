67f3cca2ea6901d6942b8f67992a6a329c97a438
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-26 08:32:22 +0300

    .

--- PhysicalLayers/testMain.py
+++ PhysicalLayers/testMain.py
@@ -91,6 +91,7 @@ def main():
             topo.nodes[0].appl.send_down(evt)
             #topo.nodes[0].appl.send_self(Event(topo.nodes[0], TestAppEventTypes.STARTREQ, None))
             time.sleep(0.01)
+        print("TIME IS UP!")
         time.sleep(20)
         #topo.nodes[2].appl.send_self(Event(topo.nodes[0], TestAppEventTypes.STARTREQ, None))
         #time.sleep(1)
