0fc6c646932edc6dedc816f0e5abd913de35f7ce
derinkaradeniz <63790219+derinkaradeniz@users.noreply.github.com>
2022-06-26 08:41:38 +0300

    .

--- PhysicalLayers/csmaPlain.py
+++ PhysicalLayers/csmaPlain.py
@@ -30,12 +30,12 @@ class CsmaPlain(GenericMac):
                     self.retrialcnt = 0
                 except Exception as e:
                     logger.critical(f"MacCsmaPPersistent handle_frame exception {e}")
-            elif self.retrialcnt < 7:
+            elif self.retrialcnt < 4:
                 self.retrialcnt = self.retrialcnt + 1
                 rand = random.random()
                 backoffCount = math.ceil(rand*(math.pow(2,self.retrialcnt)))
                 print(f"{self.componentinstancenumber}: retrial: {self.retrialcnt}: {backoffCount} ")
-                time.sleep(backoffCount*0.0001)
+                time.sleep(backoffCount*0.001)
             else:
                 eventobj = self.framequeue.get()
         else:
